1989f42e57f5f188d96d90c582eda3997cb7b5c7
Petrova Diana <petrovadianka@gmail.com>
2023-07-29 19:11:57 +0300

    Test commit

--- test1/Program.cs
+++ test1/Program.cs
@@ -10,7 +10,6 @@ namespace test1
     {
         static void Main(string[] args)
         {
-            //test comment
             Unite Diana = new Unite("Diana", 15, 10, Frashon.Goodness, 100);
             Unite Kosty = new Unite("Kosty", 25, 5, Frashon.Evile, 100);
             for (int i = 0; i < 10; i++)
